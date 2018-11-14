33f7bdc32c7502b20a4d884348fd6402b58858b6
Alexey <ultimatum424@gmail.com>
2018-11-14 18:16:48 +0300

    delete files

--- lab3_words/src/main.kt
+++
@@ -1,8 +0,0 @@
-
-
-
-fun main(args: Array<String>) {
-    val textString = "Today is very good a sunny day"
-    val wordDistance = WordDistance(textString)
-    println(wordDistance.findMaxAndMinWordSpacing("Today", "day"))
-}
