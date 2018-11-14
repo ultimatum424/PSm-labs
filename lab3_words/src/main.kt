87b8cb978923ddc740311488e7eeddce190a4f55
Alexey <ultimatum424@gmail.com>
2018-11-14 18:17:56 +0300

    add (max and min word space) lab

---
+++ lab3_words/src/main.kt
@@ -0,0 +1,8 @@
+
+
+
+fun main(args: Array<String>) {
+    val textString = "Today is very good a sunny day"
+    val wordDistance = WordDistance(textString)
+    println(wordDistance.findMaxAndMinWordSpacing("Today", "day"))
+}
